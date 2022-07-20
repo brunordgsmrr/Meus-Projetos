7b482797d08cf61096207086619faa39ab588dde
Bruno Rodrigues Moreira <bruno_rdgs_@hotmail.com>
2022-07-20 02:35:18 -0300

    Update repo

---
+++ Java/Aulas/Teste/src/application/TesteStatic.java
@@ -0,0 +1,17 @@
+package application;
+
+public class TesteStatic {
+
+	public static double PI = 3.14;
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println(soma(1,2));
+
+	}
+
+	public static double soma(int a, int b) {
+		return a + b;
+	}
+}
