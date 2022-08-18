7a948b8a3b421de364a181299d4d8b4607989b8c
Bruno Rodrigues <bruno_rdgs_@hotmail.com>
2022-08-17 22:15:51 -0300

    Adicionado diretório Livro-Java

---
+++ Livro-Java_Guia_do_Programador/README.md
@@ -0,0 +1,2 @@
+# Java - Guia do Programador 4ªEd
+## Diretório destinado a exercícios e prática do livro
---
+++ Livro-Java_Guia_do_Programador/cap02_01/src/P0201HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class P0201HelloWorld {
+
+	//Declaração do método main, que dá início ao programa
+	public static void main(String[] args) {
+		
+		//Impressão da mensagem
+		System.out.println("Hello, World!");
+	}
+
+}
